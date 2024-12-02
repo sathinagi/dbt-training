206101463f48793d7eb6aed277b83e05b34cc0ff
sathinagi@yahoo.com <sathinagi@yahoo.com>
2024-12-02 18:33:59 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+--raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit ,
+o.ordercostprice,
+o.ordersellingprice,
+--raw customers
+c.customername,
+c.segment,
+c.country,
+--raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }}  as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on p.productid = o.productid
