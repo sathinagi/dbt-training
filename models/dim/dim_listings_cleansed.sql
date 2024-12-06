3cfb14ff455264012ba50b414019e709a7b27634
sathinagi@yahoo.com <sathinagi@yahoo.com>
2024-12-06 00:56:51 +0000

    airbnb1

---
+++ models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,19 @@
+with src_listings as
+(
+select * from {{ ref('src_listings') }}
+)
+
+select 
+listing_id,
+listing_url,
+listing_name,
+room_type,
+case when minimum_nights = 0 then 1
+     else minimum_nights
+     end as minimum_nights,
+host_id,
+replace(price_str,'$') :: NUMBER(10,2) AS PRICE,
+created_at,
+updated_at
+FROM 
+src_listings
