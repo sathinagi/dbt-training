6073568ee74ce90c09d4e89cbbe910841e8f610c
Nagi Sathi <sathinagi@yahoo.com>
2024-12-09 12:00:20 +0000

    sources and seeds

---
+++ models/mart/mart_fullmoon_reviews.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with fact_reviews as
+(
+    select * from {{ ref('fct_reviews') }}
+),
+full_moon_dates as
+(
+    select * from {{ ref('full_moon_dates') }}
+)
+
+select
+r.*,
+case when fm.full_moon_date is null then 'not full moon'
+     else 'full moon'
+     end as is_full_moon
+from fact_reviews r
+left join full_moon_dates fm
+on (to_date(r.review_date) = dateadd(day,1,fm.full_moon_date))
